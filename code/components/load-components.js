29914a6533c628deb84907da00b5a6c3de0ccd3a
ArshiaSotoode <seyyedarshia.sotoode@gmail.com>
2025-03-07 04:32:21 +0330

    Add: Implement very simple chat support component with toggle functionality (note: this is only the fuctinality and the basic component)

--- code/components/load-components.js
+++ code/components/load-components.js
@@ -9,11 +9,29 @@ function loadHTMLComponent(targetElement, filePath) {
 }
 
 // Get the <nav> and <footer> elements
+const supportChat = document.getElementById("chatSupportComponent");
 const ad = document.querySelector(".ad");
 const navbar = document.querySelector("nav");
 const footer = document.querySelector("footer");
 
 // Load the nav and footer content into the corresponding elements
+loadHTMLComponent(supportChat, "./components/support.html");
 loadHTMLComponent(ad, "./components/ad.html");
 loadHTMLComponent(navbar, "./components/nav.html");
 loadHTMLComponent(footer, "./components/footer.html");
+
+// Function to toggle the visibility of the chat box
+function toggleChatBox() {
+  const chatBox = document.getElementById("chatBox");
+  const isOpen = chatBox.classList.contains("open");
+
+  if (isOpen) {
+    // Close the chat box
+    chatBox.classList.remove("open");
+    chatBox.setAttribute("aria-hidden", "true");
+  } else {
+    // Open the chat box
+    chatBox.classList.add("open");
+    chatBox.setAttribute("aria-hidden", "false");
+  }
+}
