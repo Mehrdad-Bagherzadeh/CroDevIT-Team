e2cc8cdde969d259d8906302b6399e7a77aefa1b
ArshiaSotoode <seyyedarshia.sotoode@gmail.com>
2025-03-08 23:44:01 +0330

    Add: update chat UI with scrollbar adjustments, message listener, and localized input placeholder

--- code/components/load-components.js
+++ code/components/load-components.js
@@ -1,9 +1,11 @@
+//LOAD COMPONENTS SCRIPT
 // Function to load HTML content into an element
-function loadHTMLComponent(targetElement, filePath) {
+function loadHTMLComponent(targetElement, filePath, callback) {
   fetch(filePath)
     .then((response) => response.text())
     .then((data) => {
       targetElement.innerHTML = data;
+      if (callback) callback();
     })
     .catch((error) => console.error("Error loading HTML component:", error));
 }
@@ -15,13 +17,17 @@ const navbar = document.querySelector("nav");
 const footer = document.querySelector("footer");
 
 // Load the nav and footer content into the corresponding elements
-loadHTMLComponent(supportChat, "./components/support.html");
+loadHTMLComponent(
+  supportChat,
+  "./components/support.html",
+  addChatMessageListener
+);
 loadHTMLComponent(ad, "./components/ad.html");
 loadHTMLComponent(navbar, "./components/nav.html");
 loadHTMLComponent(footer, "./components/footer.html");
 
+//CHAT SCRIPT
 // Function to toggle the visibility of the chat box
-
 function toggleChatBox() {
   const chatButton = document.getElementById("chatButton");
   const chatBox = document.getElementById("chatBox");
@@ -32,6 +38,22 @@ function toggleChatBox() {
   );
   chatButton.classList.toggle("deactivated");
 }
+
+function addChatMessageListener() {
+  const messageInput = document.getElementById("chatMessage");
+  if (messageInput) {
+    messageInput.addEventListener("keydown", function (event) {
+      if (event.key === "Enter") {
+        event.preventDefault(); // Prevent the default action (form submission)
+        sendMessage();
+      }
+    });
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  addChatMessageListener();
+});
 
 function sendMessage() {
   const messageInput = document.getElementById("chatMessage");
